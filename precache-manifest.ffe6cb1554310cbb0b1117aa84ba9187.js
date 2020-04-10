5a261dd0131579c8c1274efed0a27640fe7bfbd1
Miroljub <miroljub1995@gmail.com>
2020-04-11 00:03:27 +0200

    Updates

---
+++ precache-manifest.ffe6cb1554310cbb0b1117aa84ba9187.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69932f89ed8920f5fd936d8e9a5a4f0f",
+    "url": "/data-structures-visualization/index.html"
+  },
+  {
+    "revision": "903ac69c4c43a479a490",
+    "url": "/data-structures-visualization/static/css/2.dea17d18.chunk.css"
+  },
+  {
+    "revision": "e4b1c613a7d98ff2e9ad",
+    "url": "/data-structures-visualization/static/css/main.8fcf4080.chunk.css"
+  },
+  {
+    "revision": "903ac69c4c43a479a490",
+    "url": "/data-structures-visualization/static/js/2.e7a87f01.chunk.js"
+  },
+  {
+    "revision": "a41de4a0a2313aaf28dde17e71d8045d",
+    "url": "/data-structures-visualization/static/js/2.e7a87f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b1c613a7d98ff2e9ad",
+    "url": "/data-structures-visualization/static/js/main.b9ac9552.chunk.js"
+  },
+  {
+    "revision": "29f6c1cf7f3a75730194",
+    "url": "/data-structures-visualization/static/js/runtime-main.d5fe7503.js"
+  },
+  {
+    "revision": "610d6de99563751f6b36fa5e8050b842",
+    "url": "/data-structures-visualization/static/media/Binary_search_tree.610d6de9.png"
+  },
+  {
+    "revision": "fa005bad00ae2118cd177af6be49106f",
+    "url": "/data-structures-visualization/static/media/background.fa005bad.jpeg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/data-structures-visualization/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/data-structures-visualization/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/data-structures-visualization/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/data-structures-visualization/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/data-structures-visualization/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e7336efe8f4bfbe4b2833c1cff54edd0.js"
+  "/data-structures-visualization/precache-manifest.ffe6cb1554310cbb0b1117aa84ba9187.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/data-structures-visualization/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
